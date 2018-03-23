f46da16449d24203592654088fc17db3c6871ef5
chhavi <goel.chhavi022@gmail.com>
2018-03-23 12:16:40 +0530

    structured

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var router = require('./api');
+
+const PORT = 3009;
+
+app.use('/api', 'router');
+
+app.listen(PORT , () => {
+    console.log(`Server listening on port : ${PORT}`);
+})
